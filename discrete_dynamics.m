f06e600ea8d6beaa9d797c8b5937aaf905b86baf
Tiago-Ferreira-Git <tiagombferreira@tecnico.ulisboa.pt>
2024-06-06 09:06:40 +0100

    a step in the process

--- discrete_dynamics.m
+++ discrete_dynamics.m
@@ -1,49 +1,24 @@
-function [A,B,C,W,L,E] = discrete_dynamics(A_global,B_global,C_global,D_global,W_global,E_global,bus_ss,h,n_areas,L)
+function [A,B,W] = discrete_dynamics(A_global,B_global,W_global,h)
     
     
     G = expm([A_global W_global; zeros(size(W_global,2),size(A_global,2)) zeros(size(W_global,2),size(W_global,2))]*h);
     
-    W_global_discrete = G(1:size(A_global,1),size(A_global,2)+1:end);
+    W = G(1:size(A_global,1),size(A_global,2)+1:end);
     
+    G = expm([A_global B_global; zeros(size(B_global,2),size(A_global,2)) zeros(size(B_global,2),size(B_global,2))]*h);
     
-    sys = ss(A_global,B_global,C_global,D_global);
-    sys = c2d(sys,h);
-    
-    A = zeros(size(A_global,1)+n_areas);
-    
-    B = zeros(size(B_global,1)+n_areas,size(B_global,2));
-    %
-    C = zeros(size(C_global,1),size(C_global,2)+n_areas);
-    
-        %
-    W = zeros(size(W_global_discrete,1)+n_areas,size(W_global_discrete,2));
-    
-    E = zeros(size(E_global,1),size(E_global,2)+n_areas);
-    
-    
-    A(1:size(A_global,1),1:size(A_global,1)) = sys.A;
-    
-    A(size(A_global,1)+1:end,size(A_global,1)+1:end) = eye(n_areas);
+    B = G(1:size(A_global,1),size(A_global,2)+1:end);
 
+    A = G(1:size(A_global,1),1:size(A_global,2));
+    
 
-    %add error integrator
+    % D_global = zeros(size(A,1),size(B_global,2));
+    % 
+    % rank(ctrb(A_global,B_global))
+    % rank(ctrb(A,B))
+    % 
+    % sys = ss(A_global,B_global,eye(size(A,1)),D_global);
+    % sys = c2d(sys,h);
 
-    for i=1:n_areas
-        A(size(A_global,1)+i,sum(bus_ss(1:i-1,2))+1) = -h;
-    end
-    
-    
-    
-    B(1:size(B_global,1),1:size(B_global,2)) = sys.B;
-    
-    C(1:size(C_global,1),1:size(C_global,2)) = sys.C;
-    
-    W(1:size(W_global,1),1:size(W_global,2))  = W_global_discrete;
-    
-    E(1:size(E_global,1),1:size(E_global,2)) = E_global;
-    
-    L(1:1+size(L,1):end) = 1;
-    E(1:size(E_global,1),size(E_global,2)+1:end) = L;
 
 end
-
--- get_disturbance_profile.m
+++ get_disturbance_profile.m
@@ -1,31 +1,79 @@
-function w = get_disturbance_profile(w,h,n_areas,simulation_hours)
+function w = get_disturbance_profile(w,h,n_areas,simulation_hours,bus_ss)
     w_load_hour = load('data\w_load.mat');
     w_load_hour = w_load_hour.w_load(:,1:24);
     
     w_ren_hour = load('data\w_ren.mat');
     w_ren_hour = w_ren_hour.w_ren(:,1:24);
 
+    n_ren = sum(bus_ss(:,4));
+    %if the number of areas in the data available is lower than the areas 
     if size(w_load_hour,1) < n_areas 
         w_load_hour = [repmat(w_load_hour, floor(n_areas/size(w_load_hour,1)), 1); ...
                         w_load_hour(1:mod(n_areas, size(w_load_hour,1)),:)];
-        w_ren_hour = [repmat(w_ren_hour, floor(n_areas/size(w_ren_hour,1)), 1); ...
-                        w_ren_hour(1:mod(n_areas, size(w_ren_hour,1)),:)];
     else 
         w_load_hour = w_load_hour(1:n_areas,:);
-        w_ren_hour = w_ren_hour(1:n_areas,:);
     end
 
+    if size(w_ren_hour,1) < n_ren 
+        w_ren_hour = [repmat(w_ren_hour, floor(n_ren/size(w_ren_hour,1)), 1); ...
+                        w_ren_hour(1:mod(n_ren, size(w_ren_hour,1)),:)];
+    else 
+        w_ren_hour = w_ren_hour(1:n_ren,:);
+    end
 
+    
+    
+    %if the hours in the data available is lower than the simulation hour
     if 24 < simulation_hours 
         w_load_hour = repmat(w_load_hour,1,floor(simulation_hours/24)+1);
         w_ren_hour =  repmat(w_ren_hour,1,floor(simulation_hours/24)+1);
     end
+    
+    load_mask = zeros(1,size(w,1));
+    load_mask(1) = 1;
+    load_mask(cumsum(1+bus_ss(1:end-1,4))+1) = 1;
+    load_mask = logical(load_mask);
 
+    ren_mask = ~load_mask;
 
-    k_ = 1:1440:size(w,2);
+    k_ = 1:3600/h:size(w,2);
     for hour=1:simulation_hours
-        w(1:2:end,k_(hour):k_(hour+1)) = repmat(w_load_hour(:,hour),1,1441)*100;
-        w(2:2:end,k_(hour):k_(hour+1)) = repmat(w_ren_hour(:,hour),1,1441);
+        w(load_mask,k_(hour):k_(hour+1)) = repmat(w_load_hour(:,hour),1,3600/h +1 )*100;
+        w(ren_mask,k_(hour):k_(hour+1)) = repmat(w_ren_hour(:,hour),1,3600/h +1);
     end
+
+
+    t = 0:h:3600*simulation_hours;
+    figure
+    set(gca,'TickLabelInterpreter','latex') % Latex style axis
+    hold on
+    grid on
+    box on;
+    stairs(t,w(load_mask,:)','LineWidth',1.5);
+    legend('$\Delta P_{{load}_1}$','$\Delta P_{{load}_2}$','$\Delta P_{{load}_3}$','Interpreter','latex')
+    ylabel('$\Delta P_{load}$ (pu)','interpreter','latex');
+    xlabel('$t \;[\mathrm{s}]$','Interpreter','latex');
+    hold off
+    set(gcf,'renderer','Painters');
+    title='./fig/delta_p_load.png';
+    saveas(gca,title,'png');
+    
+    
+    if(any(ren_mask))
+        figure
+        set(gca,'TickLabelInterpreter','latex') % Latex style axis
+        hold on
+        grid on
+        box on;
+        stairs(t,w(2:2:end,:)','LineWidth',1.5);
+        legend('$\Delta P_{{ren}_1}$','$\Delta P_{{ren}_2}$','$\Delta P_{{ren}_3}$','Interpreter','latex')
+        ylabel('$\Delta P_{ren}$ (pu)','interpreter','latex');
+        xlabel('$t \;[\mathrm{s}]$','Interpreter','latex');
+        hold off
+        set(gcf,'renderer','Painters');
+        title='./fig/p_ren.png';
+        saveas(gca,title,'png');
+    end
+
 end
 
